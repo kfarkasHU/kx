c2d9db248208ca08960171aed98b56ede168b173
Kolos Farkas <kolos.farkas@betssongroup.com>
2022-09-10 13:15:51 +0200

    feat(config reader): implementation

---
+++ src/lib/contract/config.ts
@@ -0,0 +1,9 @@
+/**
+ * KxConfig definition.
+ */
+export interface KxConfig {
+	/**
+	 * Absolute path to the target directory to read the files from.
+	 */
+	workingDirectory: string,
+}
---
+++ src/lib/core/config-reader.ts
@@ -0,0 +1,46 @@
+import { KxFileEntryMode, KxFileJsonEntry } from "../../packages/common";
+import { KxConfig } from "../contract";
+
+/**
+ * KxConfigReader definition.
+ */
+export class KxConfigReader extends KxFileJsonEntry<KxConfig> {
+
+	private static _isInitialized = false;
+	
+	private _config: KxConfig;
+
+	private constructor(path: string) {
+		super(path, KxFileEntryMode.Read);
+	}
+
+	/**
+	 * Initializes the current config reader instance.
+	 * @param {string} configPath Path to the configuration file.
+	 * @returns {KxConfigReader} The configuration reader instance.
+	 */
+	public static initialize(configPath: string): KxConfigReader {
+		if(this._isInitialized) {
+			throw "KxConfigReader is already initialized.";
+		}
+		this._isInitialized = true;
+		return new KxConfigReader(configPath);
+	}
+
+	/**
+	 * Reads the configuration from the file.
+	 * @param {boolean} useCache Use cache to read/store the configuration.
+	 * @returns {KxConfig} The current configuration.
+	 */
+	public read(useCache: boolean = true): KxConfig {
+		if(useCache && !this._config) {
+			this._config = super.read();
+		}
+
+		if(useCache && this._config) {
+			return this._config;
+		}
+
+		return super.read();
+	}
+}
